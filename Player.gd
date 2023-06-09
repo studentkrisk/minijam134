5281b864d7022860354b42741c6c1249fdd94bf4
Krisk <31748409+studentkrisk@users.noreply.github.com>
2023-06-08 22:45:18 -0700

    added player

--- Player.gd
+++ Player.gd
@@ -1,3 +1,14 @@
 extends CharacterBody2D
 
-func 
+var tile_size = 16
+
+func _physics_process(delta):
+	if not $Raycast2D.is_colliding():
+		global_position += $Raycast2D.target_position
+	
+	var inp = Vector2(
+		-int(Input.is_action_just_pressed("game_left")) + int(Input.is_action_just_pressed("game_right")),
+		-int(Input.is_action_just_pressed("game_up")) + int(Input.is_action_just_pressed("game_down"))
+	)
+	
+	$Raycast2D.target_position = inp * tile_size
